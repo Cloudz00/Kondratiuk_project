6f1ee56afa249352a94b3216bea82cb8f503dd91
Cloudz00 <rubonka6@gmail.com>
2023-10-13 14:18:47 +0300

    -

---
+++ lab2/lab2_Kondratiuk.py
@@ -0,0 +1,19 @@
+print("Programming"+3*"*"+"Essential"+3*"*"+"in"+3*"."+"Python")
+
+print(4*" "+"*")
+print(3*" "+"*"+" "+"*")
+print(2*" "+"*"+3*" "+"*")
+print(" "+"*"+5*" "+"*")
+print(3*"*"+3*" "+3*"*")
+print((2*" "+"*"+3*" "+"*\n")*2, end="")
+print(2*" "+5*"*")
+
+print("I'm student")
+
+print("\"I'm\"\n"+2*"\""+"learning"+"\""*2+"\n"+3*"\""+"Python"+3*"\"")
+
+print(500**8)
+
+print(777**16)
+
+print(3**3**4)
